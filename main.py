0482714271bcef91addf629830e9d0b8035a5ae1
Admin <ghostminerx@gmail.com>
2022-05-28 09:02:08 +0700

    completed

--- main.py
+++ main.py
@@ -1,56 +1,93 @@
-import requests
-from bs4 import BeautifulSoup
+import time
+
+import selenium.common.exceptions
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import pandas as pd
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 URL = 'https://coinmarketcap.com/'
-HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/100.0.4896.143 YaBrowser/22.5.0.1814 Yowser/2.5 Safari/537.36',
-           'accept': '*/*'}
 
 
 def get_html(url):
-    r = requests.get(url=url, headers=HEADERS)
-    return r
+    browser = webdriver.Chrome()
+    browser.get(url)
+
+    return browser
 
 
-def get_content(html, currency_list):
-    soup = BeautifulSoup(html, "html.parser")
-    coins = soup.find("tbody").find_all("tr")
+def scroll(elem):
+    for i in range(10):
+        elem.send_keys(Keys.PAGE_DOWN)
+        time.sleep(0.2)
 
-    for coin in coins:
-        print(coin)
-        name = coin.find(class_="cmc-link").get("href").replace("/currencies/", "")[:-1]
-        price = coin.find(class_="sc-131di3y-0 cLgOOr").find('span')
-        print(price)
-        #price = price.get_text()
-        market_cap = coin.find(class_="sc-1ow4cwt-1 ieFnWP")
-        #market_cap = market_cap.get_text()
-        if price:
-            currency_list.append({
-                'Name': name,
-                'Price': price.text,
-                'Market-Cap': market_cap.text
-            })
-        else:
+
+def get_content(browser: webdriver, cur_list):
+    close = browser.find_element(By.XPATH, value='//*[@id="cmc-cookie-policy-banner"]/div[2]')
+    close.click()
+    browser.maximize_window()
+    coins = browser.find_element(by=By.TAG_NAME, value='body')
+    scroll(coins)
+
+    coins = browser.find_element(by=By.TAG_NAME, value='tbody')
+    coins = coins.text.split('\n')
+    iter = 1
+    xpath = '//*[@id="__next"]/div/div[1]/div[2]/div/div[1]/div[7]/div[1]/div/ul/li[10]/a'
+    while True:
+        time.sleep(0.5)
+        if iter == 5:
+            xpath = '//*[@id="__next"]/div/div[1]/div[2]/div/div[1]/div[7]/div[1]/div/ul/li[11]/a'
+        try:
+            button = browser.find_element(by=By.XPATH,
+                                          value=xpath)# browser.find_element(by=By.CLASS_NAME, value='chevron')
+
+            button.click()
+        except selenium.common.exceptions.NoSuchElementException:
             break
-    print_list(currency_list)
+        next_coins = browser.find_element(by=By.TAG_NAME, value='body')
+        scroll(next_coins)
+        next_coins = browser.find_element(by=By.TAG_NAME, value='tbody')
+        next_coins = next_coins.text.split('\n')
+        for elem in next_coins:
+            coins.append(elem)
+        iter += 1
+    i = 0
+    print(coins)
+    while i < 10045:
+        i += 1
+        try:
+            name_index = coins.index(str(i)) + 1
+            name = coins[name_index]
+
+            price_index = coins.index(str(i)) + 3
+            price = coins[price_index]
+
+            market_cap_index = coins.index(str(i)) + 5
+            market_cap = coins[market_cap_index]
+
+            cur_list.append({
+                'Name': name,
+                'Price': price,
+                'Market-Cap': market_cap,
+            })
+        except ValueError:
+            continue
 
 
-def print_list(list):
-    print('Name\t Price\t Market-Cap')
-    for string in list:
-        print(string['Name'] + ":  " + string['Price'] + "   " + string['Market-Cap'])
+def write(cur_list):
+    cur_list = pd.DataFrame(cur_list)
+    #print(cur_list)
+    cur_list.to_csv('result.csv')
 
 
-def parse(currency_list):
+def parse(cur_list):
     html = get_html(URL)
-    if html.status_code == 200:
-        get_content(html.text, currency_list)
-    else:
-        print("Something went wrong"
-              "Traceback: parse()")
+    get_content(html, cur_list)
+    write(cur_list)
 
 
 if __name__ == '__main__':
     currency_list = []
     parse(currency_list)
-
